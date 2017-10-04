44f879445268da48286f6c323aca75665058de06
Lionel HERVIER <lhervier@asi.fr>
2017-10-04 18:18:51 +0200

    [Backend Domino] No more oauth2.client.database notes.ini variable

--- backends/domino-osgi/com.github.lhervier.domino.oauth.client/src/com/github/lhervier/domino/oauth/client/Oauth2ClientDbPropertySource.java
+++ backends/domino-osgi/com.github.lhervier.domino.oauth.client/src/com/github/lhervier/domino/oauth/client/Oauth2ClientDbPropertySource.java
@@ -1,21 +1,12 @@
 package com.github.lhervier.domino.oauth.client;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import lotus.domino.Database;
 import lotus.domino.NotesException;
-import lotus.domino.Session;
 
 import com.github.lhervier.domino.spring.servlet.BaseParamViewPropertySource;
 
 public class Oauth2ClientDbPropertySource extends BaseParamViewPropertySource {
 
-	/**
-	 * Authorized databases
-	 */
-	private Set<String> databases = new HashSet<String>();
-	
 	/**
 	 * Constructor
 	 */
@@ -40,26 +31,10 @@ public class Oauth2ClientDbPropertySource extends BaseParamViewPropertySource {
 	}
 
 	/**
-	 * @see com.github.lhervier.domino.spring.servlet.BaseParamViewPropertySource#init(lotus.domino.Session)
-	 */
-	@Override
-	public void init(Session sessionAsServer) throws NotesException {
-		super.init(sessionAsServer);
-		
-		String dbs = sessionAsServer.getEnvironmentString("oauth2.client.databases", true);
-		if( dbs == null || dbs.length() == 0 )
-			throw new RuntimeException("The 'oauth2.client.databases' must be defined in the notes.ini. It must contains the names of the oauth2 client databases.");
-		String[] tblDbs = dbs.split(",");
-		for( String db : tblDbs )
-			this.databases.add(db.replace('\\', '/').trim());
-	}
-
-	/**
-	 * Check if the database is elligible to extract properties.
-	 * OAUTH2 Client database must be declared in the notes.ini
+	 * @see com.github.lhervier.domino.spring.servlet.BaseParamViewPropertySource#checkDb(lotus.domino.Database)
 	 */
 	@Override
 	public boolean checkDb(Database database) throws NotesException {
-		return this.databases.contains(database.getFilePath().replace('\\', '/'));
+		return true;
 	}
 }
